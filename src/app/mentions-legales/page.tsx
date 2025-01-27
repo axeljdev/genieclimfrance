8355f6d16cccdf20394bb9846bd432f4efaaa984
Axel <axeljpro@gmail.com>
2025-01-27 09:47:45 +0100

    fix img

--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -1,7 +1,7 @@
 export default function MentionsLegales() {
   return (
     <>
-      <header className="bg-[url('/hero-mobile.png')] bg-cover bg-center flex flex-col justify-center items-center lg:items-start lg:font-kollektif lg:pl-20 text-tertiary py-8 h-[300px]">
+      <header className="bg-[url('/hero-mobile.webp')] bg-cover bg-center flex flex-col justify-center items-center lg:items-start lg:font-kollektif lg:pl-20 text-tertiary py-8 h-[300px]">
         <h1 className="text-3xl font-bold mb-8">Mentions légales</h1>
       </header>
       <div className="max-w-6xl mx-auto px-4 py-8">
