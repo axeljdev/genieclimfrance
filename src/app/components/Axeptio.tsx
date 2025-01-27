9dd6cf4f6ddb4bc2ec2d20a6889d6fcff60e59e7
Axel <axeljpro@gmail.com>
2025-01-27 09:57:29 +0100

    fix progress bar and axeptio

--- src/app/components/Axeptio.tsx
+++ src/app/components/Axeptio.tsx
@@ -1,43 +1,39 @@
 "use client";
 
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function Axeptio() {
-  useEffect(() => {
-    const script = document.createElement("script");
+  const AXEPTIO_CLIENT_ID = process.env.NEXT_PUBLIC_AXEPTIO_CLIENT_ID;
+  const AXEPTIO_VERSION = process.env.NEXT_PUBLIC_AXEPTIO_VERSION;
 
-    const AXEPTIO_CLIENT_ID = process.env.NEXT_PUBLIC_AXEPTIO_CLIENT_ID;
-    const AXEPTIO_VERSION = process.env.NEXT_PUBLIC_AXEPTIO_VERSION;
-
-    script.innerHTML = `
-      window.axeptioSettings = {
-        clientId: "${AXEPTIO_CLIENT_ID}",
-        cookiesVersion: "${AXEPTIO_VERSION}",
-        googleConsentMode: {
-          default: {
-            analytics_storage: "denied",
-            ad_storage: "denied",
-            ad_user_data: "denied",
-            ad_personalization: "denied",
-            wait_for_update: 500
-          }
-        }
-      };
-      
-      (function(d, s) {
-        var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-        e.async = true;
-        e.src = "https://static.axept.io/sdk.js";
-        e.integrity = "sha384-VOTRE-HASH-ICI";
-        t.parentNode.insertBefore(e, t);
-      })(document, "script");
-    `;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return null;
+  return (
+    <>
+      <Script
+        id="axeptio-config"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.axeptioSettings = {
+              clientId: "${AXEPTIO_CLIENT_ID}",
+              cookiesVersion: "${AXEPTIO_VERSION}",
+              googleConsentMode: {
+                default: {
+                  analytics_storage: "denied",
+                  ad_storage: "denied",
+                  ad_user_data: "denied",
+                  ad_personalization: "denied",
+                  wait_for_update: 500
+                }
+              }
+            };
+          `,
+        }}
+      />
+      <Script
+        src="https://static.axept.io/sdk.js"
+        strategy="afterInteractive"
+        id="axeptio-sdk"
+      />
+    </>
+  );
 }
