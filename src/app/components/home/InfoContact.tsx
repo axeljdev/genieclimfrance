28d608e1aca0e902ae86592b722bcb4fbe8d3c62
Axel <axeljpro@gmail.com>
2025-02-17 09:24:05 +0100

    fix phone number

--- src/app/components/home/InfoContact.tsx
+++ src/app/components/home/InfoContact.tsx
@@ -2,7 +2,7 @@ function InfoContact() {
   return (
     <div className="lg:flex lg:flex-col gap-4 p-6 lg:w-1/3 bg-secondary text-tertiary hidden rounded-3xl">
       <h3 className="text-2xl font-emOne text-tertiary">Info contact</h3>
-      <p>Téléphone: +33 9 72 12 14 02</p>
+      <p>Téléphone: +33 9 72 12 14 01</p>
       <p>Email: contact@genieclimfrance.fr</p>
       <h4 className="font-bold text-primary uppercase">
         génieclim france - Siège social
